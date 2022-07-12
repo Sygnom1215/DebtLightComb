41ab4ffbcd71653437f6826bdc712cba72a49e42
박소형 <77712386+Sygnom1215@users.noreply.github.com>
2022-07-13 00:48:05 +0900

    07/13 Invoke 활용하여 고양이 출현에 딜레이 줬음. 고양이를 클릭하면 다시 꺼지는 것도 만들어야 함

--- Assets/02.Scirpts/UI/RandomAppear.cs
+++ Assets/02.Scirpts/UI/RandomAppear.cs
@@ -8,20 +8,32 @@ public class RandomAppear : MonoBehaviour
     [SerializeField]
     private GameObject catBox;
 
+    private bool isAppear;
+
     void Start()
     {
         catBox.SetActive(false);
+        isAppear = false;
     }
     void Update()
     {
-        StartCoroutine(Appear());
+        //StartCoroutine(Appear());
+        if (isAppear == false)
+            Invoke("AppearCat", Random.Range(5.0f, 17.0f));
+
 
     }
-    private IEnumerator Appear()
+
+    //private IEnumerator Appear()
+    //{
+    //    catBox.SetActive(true);
+    //    yield return new WaitForSeconds(Random.Range(5.0f, 17.0f));
+    //    catBox.SetActive(false);
+    //}
+
+    private void AppearCat()
     {
         catBox.SetActive(true);
-        yield return new WaitForSeconds(Random.Range(5.0f, 17.0f));
-        catBox.SetActive(false);
-
+        isAppear = true;
     }
 }
