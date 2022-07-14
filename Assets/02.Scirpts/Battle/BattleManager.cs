740ccbc5259e263737f7c93e1e8bbeef654185ec
LLL <77712364+elsam0104@users.noreply.github.com>
2022-07-14 17:59:03 +0900

    0714 배틀 메니저 생성

--- Assets/02.Scirpts/Battle/BattleManager.cs
+++ Assets/02.Scirpts/Battle/BattleManager.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class BattleManager : MonoBehaviour
 {
     [SerializeField]
     private BattleSO battleSO;
     [SerializeField]
     private PlayerSO playerSO;
-
-    private void Damage()
+    [SerializeField]
+    private Transform cameraTransform;
+    private void PlayerAttack()
     {
-
+        battleSO.hp -= playerSO.damage;
+        cameraTransform.DOShakePosition(0.3f, 0.1f);
     }
 }
